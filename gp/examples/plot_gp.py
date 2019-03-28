bb90c5a3d08cde7a15f3b068aa956342b05aa81d
Samuel Murray <samuelmu@kth.se>
2019-03-28 16:01:42 +0100

    Change to use no function by default, i.e. make plot completely interactive

--- gp/examples/plot_gp.py
+++ gp/examples/plot_gp.py
@@ -1,4 +1,3 @@
-import numpy as np
 from matplotlib import pyplot as plt
 
 from gp.visualisation import util, GP_Plotter
@@ -6,9 +5,9 @@ from gp.kernel import *
 from gp.model import GP
 
 if __name__ == "__main__":
-    func = util.sin
+    func = None  # This can be changed to e.g. util.sin
     kern = RBF(-1, -1)
     gp = GP(kern=kern)
-    gplot = GP_Plotter(gp, func=func)
+    gplot = GP_Plotter(gp, func)
     gplot.plot_posterior()
     plt.show()
