7ffa35f74e5a389165839cdf5ff613998a5fd6f7
Samuel Murray <samuelmu@kth.se>
2019-04-05 16:28:55 +0200

    Add type hints for x and return value

--- gp/visualisation/util.py
+++ gp/visualisation/util.py
@@ -1,33 +1,33 @@
 import numpy as np
 
 
-def sin(x):
+def sin(x: np.ndarray) -> np.ndarray:
     return np.sin(x)
 
 
-def cos(x):
+def cos(x: np.ndarray) -> np.ndarray:
     return np.cos(x)
 
 
-def sin2(x):
+def sin2(x: np.ndarray) -> np.ndarray:
     return np.square(np.sin(x)) - 0.5
 
 
-def sincos(x):
+def sincos(x: np.ndarray) -> np.ndarray:
     return np.sin(x) * np.cos(x)
 
 
-def lin(x):
+def lin(x: np.ndarray) -> np.ndarray:
     return 0.1 * x + np.random.normal(0, 0.1)
 
 
-def expsin(x):
+def expsin(x: np.ndarray) -> np.ndarray:
     return np.exp(-np.square(x)) * np.sin(x)
 
 
-def step(x):
+def step(x: np.ndarray) -> np.ndarray:
     return (x > 0) - 0.5
 
 
-def relu(x):
+def relu(x: np.ndarray) -> np.ndarray:
     return 0.01 + np.max((0, x))
