9ded873806e375a00b43fc42455157ebc6424a50
Samuel Murray <samuelmu@kth.se>
2019-04-05 17:35:44 +0200

    Add static method _check_input_is_valid that raises exception if input are of different dimensions

--- gp/kernel/kernel.py
+++ gp/kernel/kernel.py
@@ -17,6 +17,12 @@ class Kernel(ABC):
             raise ValueError(f"Vectors must be of matching dimension, "
                              f"but x1.shape = {x1.shape} and x2.shape = {x2.shape}")
 
+    @staticmethod
+    def _check_input_is_valid(x1: np.ndarray, x2: np.ndarray) -> None:
+        if x1.shape[1] != x2.shape[1]:
+            raise ValueError(f"Vectors must be of matching dimension, "
+                             f"but x1.shape = {x1.shape} and x2.shape = {x2.shape}")
+
     def set_params(self, params: np.ndarray) -> None:
         if params.size != self.nparams:
             raise ValueError(f"Provided {params.size} params; must be {self.nparams}")
